c70ba816b6b30051e8e082c793908fd6a5919371
Marc Galera <mgaleramunoz@gmail.com>
2023-03-22 11:34:44 +0100

    refactor: 'refactor script'
    Add documentation + delete unused lines of code

--- eleccions_pipeline_backend.py
+++ eleccions_pipeline_backend.py
@@ -1,35 +1,30 @@
-# Import dependencies
-import requests
-import dotenv
-import pandas as pd
+'''
+Description: Script to download files from a remote SFTP folder, read them as dataframes, to parse them finally as json files.
+    Finally a patch request to airtable is done to update records into airtable.
+'''
 
+# Import dependencies
+import os
+import dotenv
 from ftp import load_file_from_ftp
 from llistes_nacionals_parroquials import create_lists
 from airtable import save_to_airtable
-#---------------------------------------------------------------------------------
 
-# Import env vars:
+# Load env vars:
 dotenv.load_dotenv()
+remote_path = os.getenv('folder_path')
+files = os.getenv('file_names')
 
-    #airtable related env vars:
-# airtable_api_url = os.getenv('airtable_api_url')
-# airtable_api_key = os.getenv('airtable_api_key')
-
-
-#---------------------------------------------------------------------------------
-files = load_file_from_ftp('/rtva/', ['Escrutini_Parroquial.xls'])
-# file = './Escrutini_Parroquial.xls'
+files = load_file_from_ftp(remote_path, files)
 df = files[0]
-lists = create_lists(df)
-
-print(lists)
-
+lists = create_lists(df); print(lists)
 save_to_airtable(lists[:10])
 
 #TESTING!!
-#---------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------
     #In case we have a second file containing participation stats, we want to retrieve the blanks and nulls:
 #bdf_blanks_nulls = pd.read_excel(sftp.open(remote_file_2))
 #Appropiate treatment:
 
+
+#2. Reformat code: generalize second file + upload records and upload other pending records
+#3. Push to CloudFunction and scheduler
