5f39dbdb50a7bf8bf50fb8952794df43d9b3aa00
Marc Galera <mgaleramunoz@gmail.com>
2023-03-22 11:56:29 +0100

    fix: 'the dir and names path was not working'

--- eleccions_pipeline_backend.py
+++ eleccions_pipeline_backend.py
@@ -4,22 +4,21 @@ Description: Script to download files from a remote SFTP folder, read them as da
 '''
 
 # Import dependencies
-import os
-import dotenv
 from ftp import load_file_from_ftp
 from llistes_nacionals_parroquials import create_lists
 from airtable import save_to_airtable
 
-# Load env vars:
-dotenv.load_dotenv()
-remote_path = os.getenv('folder_path')
-files = os.getenv('file_names')
+# Define script parameters:
+remote_path = '/rtva/'
+files = ['Escrutini_Parroquial.xls'] 
 
 files = load_file_from_ftp(remote_path, files)
 df = files[0]
 lists = create_lists(df); print(lists)
-save_to_airtable(lists[:10])
 
+save_to_airtable(lists[:10])
+save_to_airtable(lists[11:21])
+save_to_airtable(lists[22:26])
 #TESTING!!
     #In case we have a second file containing participation stats, we want to retrieve the blanks and nulls:
 #bdf_blanks_nulls = pd.read_excel(sftp.open(remote_file_2))
