558da5b347a85686188f53d90415b4883b2f91f5
Marc Galera <mgaleramunoz@gmail.com>
2023-03-23 16:15:23 +0100

    feat: 'add second function to process blank and null votes'

--- airtable.py
+++ airtable.py
@@ -21,7 +21,23 @@ def save_to_airtable(records):
     }
 
     print(airtable_api_key, ' ', airtable_api_url)
-    response = requests.patch(airtable_api_url + '/lists', headers=headers, data=json.dumps({ 'records': records}))
+    response = requests.patch(airtable_api_url + '/lists', headers=headers, data=json.dumps({'records': records}))
+    if response.status_code == 200:
+        print("Record updated successfully")
+    else:
+        print("Error updating record: " + json.dumps(response.json()))
+
+def save_to_airtable_2(records):
+    '''
+    Description: Updates records in Airtable from dictionaries, parsed as json file
+    Arguments:
+        - 'records'(list): list of dictionaries with the records to be updated
+    '''
+    headers = {
+        "Authorization": f"Bearer {airtable_api_key}",
+        "Content-Type": "application/json"
+    }
+    response = requests.patch(airtable_api_url + '/results', headers=headers, data=json.dumps({'records': records}))
     if response.status_code == 200:
         print("Record updated successfully")
     else:
